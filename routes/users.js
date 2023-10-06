30d7bce04d34ddc0cc100ef7ed337e8db93b1bf0
papernoguy <114077912+papernoguy@users.noreply.github.com>
2023-10-06 11:11:28 +0300

    script to populate the users on the db

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,28 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const app = express();
+const router = express.Router();
+const userModel = require("../models/user");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async (request, response) => {
+  const users = await userModel.find({});
+
+  try {
+    response.send(users);
+  } catch (error) {
+    response.status(500).send(error);
+  }
+});
+
+router.post('/add_user', async (req, res) => {
+  const user = new userModel(req.body);
+
+  try {
+    await user.save();
+    res.send(user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 module.exports = router;
